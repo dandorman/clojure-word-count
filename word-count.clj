062193f68573a75dfdceec9619087d0f21fe6d48
Dan Dorman <dan.dorman@gmail.com>
2013-09-24 11:58:25 -0600

    Replace my stuff with Clojure's frequencies

--- word-count.clj
+++ word-count.clj
@@ -1,13 +1,8 @@
 (ns phrase
-  (:use [clojure.string :only [lower-case replace split]]))
+  (:use [clojure.string :only [lower-case split]]))
 
 (defn word-count
   [string]
-  (let [normalized-string (replace (lower-case string) #"[^a-z0-9 ]" "")
+  (let [normalized-string (clojure.string/replace (lower-case string) #"[^a-z0-9 ]" "")
         words (split normalized-string #"\s+")]
-    (reduce (fn [counts word]
-              (if (nil? (get counts word))
-                (assoc counts word 1)
-                (assoc counts word (inc (get counts word)))))
-            {}
-            words)))
+    (frequencies words)))
